5331bd8e65634ebfc0bb804b33c836e6510d0db5
kkiyama117 <k.kiyama117@gmail.com>
2019-06-25 16:13:55 +0900

    Update

--- codes/factories/usecases/create_user.go
+++ codes/factories/usecases/create_user.go
@@ -1,15 +1,15 @@
 package usecases
 
 import (
-	controllers2 "pumpkin/codes/interface_adapters/create_user/controllers"
-	driver_ports2 "pumpkin/codes/interface_adapters/create_user/driver_ports"
-	presenters2 "pumpkin/codes/interface_adapters/create_user/presenters"
+	"pumpkin/codes/interface_adapters/create_user/controllers"
+	"pumpkin/codes/interface_adapters/create_user/driver_ports"
+	"pumpkin/codes/interface_adapters/create_user/presenters"
 	"pumpkin/codes/usecases/create_user/interactor"
 )
 
-func InjectedCreateUser(output driver_ports2.APIOutput) controllers2.CreateUserController {
-	pres := presenters2.NewPresenter(output)
+func InjectedCreateUser(output driver_ports.APIOutput) controllers.CreateUserController {
+	pres := presenters.NewPresenter(output)
 	inter := interactor.NewInteractor(pres)
-	c := controllers2.NewController(inter)
+	c := controllers.NewController(inter)
 	return c
 }
